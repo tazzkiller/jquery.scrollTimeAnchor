9f220b1a5b162563be57fdac3718fe10f851e890
tazzkiller <tazzkiller@hotmail.com>
2012-04-30 16:04:39 +0200

    first commit
    test

---
+++ js/jquery.scrollTimeAnchor.js
@@ -0,0 +1,52 @@
+/**
+ * scrollTimeAnchor plugin for jQuery
+ * v1.0
+ * Move to anchor with a regular scroll time
+ *
+ * By Tazzkiller, creanet3d.com
+ *
+ * Please use as you wish at your own risk.
+ */
+
+/**
+ * Usage:
+ *
+ * From JavaScript, use:
+ *     $(<select>).scrollTimeAnchor({duration: '<A>', wrapper: '<B>', top: '<C>', easing: '<D>' });
+ *     where:
+ *       <select> is the DOM node selector, e.g. ".scroll"
+ *       <A> Duration of total scroll (default = '10000' )
+ *       <B> Id of the container for scroll ( Important !! fixed height,  default = '#main' )
+ *       <C> Distance to the top for stop scroll (default = '300' )
+ *       <D> include plug in easing for easing (default = 'swing' )
+ */
+
+(function($) {
+
+	$.fn.scrollTimeAnchor = function(params) {
+
+		params = $.extend( {duration: '10000', wrapper: '#main', top: '300', easing: 'swing' }, params);
+
+		this.click(function() {
+		var $t = $(this);
+	   var elementClicked = $t.attr("href");
+	   var elementCourant = window.pageYOffset;
+	   var winHeight = $(params.wrapper).outerHeight();
+	   var destination = $(elementClicked).offset().top;
+	   var totalCourant = winHeight - elementCourant;
+	   if ( params.duration > winHeight ) {
+	   var scrollRatio = winHeight / params.duration;
+	   }
+	   else { var scrollRatio = params.duration / winHeight; }
+	   var totalClicked = winHeight - destination;
+	   if ( totalCourant > totalClicked ) { var durationScroll = ((totalCourant - totalClicked) * scrollRatio);} 
+	   else { var durationScroll = ((totalClicked - totalCourant) * scrollRatio);}
+	   $("html,body").animate(
+	   { scrollTop: destination - params.top}, 
+	   {duration: durationScroll,easing:params.easing});
+	   return false;
+		});
+		return this;
+		};
+
+})(jQuery);
